39f4f583c3408d1e0b71fb23d3de77918550fbf9
MertUyar <uyarmmert@gmail.com>
2026-08-12 15:46:35 +0200

    sac_1

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ def main(config):
         elif merged_config["ALG"] == "RAINBOW":  
             from agents.rainbow.rainbow import single_run  
             run_fn = single_run
+        elif merged_config["ALG"] == "SAC":  
+            from agents.sac.sac import single_run  
+            run_fn = single_run
 
         used_seed = starting_seed + seed
         print(f"Running seed {used_seed} ...")
